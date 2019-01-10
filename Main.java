64402681c46beacaddb631a2f14773090712bfa9
frs2000 <46113874+frs2000@users.noreply.github.com>
2019-01-10 11:02:22 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,16 +2,6 @@ package ElevatorTestTask;
 
 public class Main {
     public static void main(String[] args) {
-        Elevator elevator = new Elevator ();
-        
-        Person person1 = new Person(1, 4);
-        Person person2 = new Person(3, 2);
-        Person person3 = new Person(4, 1); 
-
-        elevator.addPerson(person1);
-        elevator.addPerson(person2); 
-        elevator.addPerson(person3);
-    
-    //    elevator.start();
+        Building building = new Building();
     }
 }
