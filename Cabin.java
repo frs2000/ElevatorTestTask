5536483e7073c31a1ad5e2da9daee7c76cbba96b
frs2000 <46113874+frs2000@users.noreply.github.com>
2019-01-10 11:12:32 +0200

    Added new logic

--- Cabin.java
+++ Cabin.java
@@ -1,13 +1,21 @@
 package ElevatorTestTask;
 
 public class Cabin {
+
     private boolean door;
     private int numberOfPeopleInCabin = 0;
+    private int maxTotalPeopleWeight = 700;
+    private int onePeopleWeight = 70;
+    private int maxNumberOfPiople = maxTotalPeopleWeight / onePeopleWeight; // = 10 
 
     public void openCloseDoor() {
         door = !door;
-        if (door == false) System.out.println("Request. Door closed");
-        if (door == true) System.out.println("Request. The door is open");
+        if (door == false) {
+            System.out.println("Request. Door closed");
+        }
+        if (door == true) {
+            System.out.println("Request. The door is open");
+        }
     }
 
     public boolean getOpenClosed() {
@@ -20,5 +28,9 @@ public class Cabin {
         } else {
             System.out.println("The door is open");
         }
+    }
+
+    public int getMaxNumberOfPiople() {
+        return maxNumberOfPiople;
     }
 }
