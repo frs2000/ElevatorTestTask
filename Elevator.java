86f44df261db100fb7fc4e81e028f46564ebacca
frs2000 <46113874+frs2000@users.noreply.github.com>
2019-01-10 11:08:11 +0200

    Expanded logic

--- Elevator.java
+++ Elevator.java
@@ -1,26 +1,37 @@
 package ElevatorTestTask;
 
 public class Elevator {
+
     private Cabin cabin;
     private Controller controller;
 
-    public Elevator() {
-        cabin = new Cabin();
+    public Elevator(int elevatorShaft) {
         controller = new Controller();
-        controller.getCabinLabel(cabin);
+        for (int i = 0; i < elevatorShaft; i++) {   
+            cabin = new Cabin();                    
+            controller.getCabinLabel(cabin);        
+        }       
     }
 
     public void addPerson(Person person) {
         controller.addPerson(person);
     }
-    
+
     public void start() {
         controller.start();
     }
-    
+
     public void stop(int floor, String upDown) {
         controller.stop(floor, upDown);
-    } 
+    }
+
+    public void sendNumberOfFloors(int maxCabinFloor) {
+        controller.setMaxCabinFloor(maxCabinFloor);
+    }
+
+    public void sendNumberOfCabinToController(int numberOfCabin) {
+        controller.getNumberOfCabinToController(numberOfCabin);
+    }
 }
 
 
